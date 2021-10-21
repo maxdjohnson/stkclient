c345acc8e210102c77497d51ab01a4d3c1904e53
Max Johnson <mjohnson@plaid.com>
2021-10-20 22:43:09 -0700

    api impl

--- src/sendtokindle/main.py
+++ src/sendtokindle/main.py
@@ -23,13 +23,13 @@ class OwnedDevice:
 
 
 @dataclasses.dataclass(frozen=True)
-class OwnedDevicesResponse:
+class GetOwnedDevicesResponse:
     owned_devices: List[OwnedDevice]
     status_code: int
 
     @staticmethod
-    def from_dict(d: Mapping[str, Any]) -> 'OwnedDevicesResponse':
-        return OwnedDevicesResponse(
+    def from_dict(d: Mapping[str, Any]) -> 'GetOwnedDevicesResponse':
+        return GetOwnedDevicesResponse(
             owned_devices=[OwnedDevice.from_dict(v) for v in d["ownedDevices"]],
             status_code=d["statusCode"],
         )
@@ -54,7 +54,7 @@ def main():
     print(res)
 
 
-def get_list_of_owned_devices(s: Signer) -> OwnedDevicesResponse:
+def get_list_of_owned_devices(s: Signer) -> GetOwnedDevicesResponse:
     path = "/GetListOfOwnedDevices"
     data = json.dumps({
         "ClientInfo": {
@@ -77,7 +77,110 @@ def get_list_of_owned_devices(s: Signer) -> OwnedDevicesResponse:
     except:
         print(r.text)
         raise
-    return OwnedDevicesResponse.from_dict(r.json())
+    return GetOwnedDevicesResponse.from_dict(r.json())
+
+@dataclasses.dataclass(frozen=True)
+class GetUploadUrlResponse:
+    expiry_time: int
+    status_code: int
+    stk_token: str
+    upload_url: str
+
+    @staticmethod
+    def from_dict(d: Mapping[str, Any]) -> 'GetUploadUrlResponse':
+        return GetUploadUrlResponse(
+            expiry_time=d["expiryTime"],
+            status_code=d["statusCode"],
+            stk_token=d["stkToken"],
+            upload_url=d["uploadUrl"],
+        )
+
+def get_upload_url(s: Signer, file_size: number) -> GetUploadUrlResponse:
+    path = "/GetUploadUrl"
+    data = json.dumps({
+        "ClientInfo": {
+            "appName": "ShellExtension",
+            "appVersion": "1.1.1.253",
+            "os": "MacOSX_10.14.6_x64",
+            "osArchitecture": "x64"
+        },
+        "fileSize": file_size,
+    }, indent=4)
+    r = requests.post("https://stkservice.amazon.com" + path, headers={
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-ADP-Request-Digest': s.digest_header_for_request('POST', path, data),
+        'X-ADP-Authentication-Token': s.adp_token,
+        'Accept-Language': 'en-US,*',
+        'User-Agent': 'Mozilla/5.0',
+    }, data=data, verify=False)
+    try:
+        r.raise_for_status()
+    except:
+        print(r.text)
+        raise
+    return GetUploadUrlResponse.from_dict(r.json())
+
+def upload_file(url: string, path: string) -> None:
+    with open(path, 'rb') as f:
+        r = requests.put(url, headers={
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'en-US,*',
+            'User-Agent': 'Mozilla/5.0',
+        }, data=f)
+    try:
+        r.raise_for_status()
+    except:
+        print(r.text)
+        raise
+
+@dataclasses.dataclass(frozen=True)
+class SendToKindleResponse:
+    sku: str
+    status_code: int
+
+    @staticmethod
+    def from_dict(d: Mapping[str, Any]) -> 'GetUploadUrlResponse':
+        return GetUploadUrlResponse(
+            sku=d["sku"],
+            status_code=d["statusCode"],
+        )
+
+def send_to_kindle(s: Signer, stk_token: str, target_device_serial_numbers: List[str], *, author: str, title: str, format: str = "mobi") -> SendToKindleResponse:
+    path = "/SendToKindle"
+    data = json.dumps({
+        "ClientInfo": {
+            "appName": "ShellExtension",
+            "appVersion": "1.1.1.253",
+            "os": "MacOSX_10.14.6_x64",
+            "osArchitecture": "x64"
+        },
+        "DocumentMetadata": {
+            "author": author,
+            "crc32": 0,
+            "inputFormat": format,
+            "title": title,
+        },
+        "archive": True,
+        "deliveryMechanism": "WIFI",
+        "outputFormat": "MOBI",
+        "stkToken": stk_token,
+        "targetDevices": target_devices,
+    }, indent=4)
+    r = requests.post("https://stkservice.amazon.com" + path, headers={
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-ADP-Request-Digest': s.digest_header_for_request('POST', path, data),
+        'X-ADP-Authentication-Token': s.adp_token,
+        'Accept-Language': 'en-US,*',
+        'User-Agent': 'Mozilla/5.0',
+    }, data=data, verify=False)
+    try:
+        r.raise_for_status()
+    except:
+        print(r.text)
+        raise
+    return SendToKindleResponse.from_dict(r.json())
 
 
 if __name__ == "__main__":
