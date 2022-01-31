c0385afd79e4ec73a776850abe02662eeea913d1
Max Johnson <maxdjohnson@gmail.com>
2022-01-30 16:27:53 -0800

    auth

---
+++ src/stkclient/auth.py
@@ -0,0 +1,88 @@
+"""Send To Kindle Authorization."""
+
+import base64
+import hashlib
+import json
+import os
+import urllib.parse
+import urllib.request
+
+from stkclient.client import Client
+
+
+class OAuth2:
+    """Authenticates an end-user using amazon's OAuth2."""
+
+    def __init__(self):
+        """Constructs an OAuth2."""
+        self._verifier = _base64_url_encode(os.urandom(32))
+
+    def get_signin_url(self) -> str:
+        """Gets the signin URL."""
+        challenge = _base64_url_encode(_sha256(self._verifier.encode("utf-8")))
+        q = {
+            "openid.claimed_id": "http://specs.openid.net/auth/2.0/identifier_select",
+            "openid.ns.oa2": "http://www.amazon.com/ap/ext/oauth/2",
+            "openid.ns": "http://specs.openid.net/auth/2.0",
+            "openid.identity": "http://specs.openid.net/auth/2.0/identifier_select",
+            "openid.oa2.client_id": "device:658490dfb190e494030082836775981fa23be0c2425441860352ba0f55915b43002d",
+            "openid.mode": "checkid_setup",
+            "openid.oa2.scope": "device_auth_access",
+            "openid.oa2.response_type": "code",
+            "openid.oa2.code_challenge": challenge,
+            "openid.oa2.code_challenge_method": "S256",
+            "openid.return_to": "https://www.amazon.com/gp/sendtokindle",
+            "openid.ns.pape": "http://specs.openid.net/extensions/pape/1.0",
+            "openid.pape.max_auth_age": "0",
+            "accountStatusPolicy": "P1",
+            "openid.assoc_handle": "amzn_device_na",
+            "pageId": "amzn_device_common_dark",
+            "disableLoginPrepopulate": "1",
+        }
+        return "https://www.amazon.com/ap/signin?" + urllib.parse.urlencode(q)
+
+    def create_client(self, redirect_url: str) -> Client:
+        """Creates a client with the authorization code from the redirect url."""
+        u = urllib.parse.urlparse(redirect_url)
+        q = urllib.parse.parse_qs(u.query)
+        code = q["openid.oa2.authorization_code"][0]
+        body = {
+            "app_name": "Unknown",
+            "client_domain": "DeviceLegacy",
+            "client_id": "658490dfb190e494030082836775981fa23be0c2425441860352ba0f55915b43002d",
+            "code_algorithm": "SHA-256",
+            "code_verifier": self._verifier,
+            "requested_token_type": "access_token",
+            "source_token": code,
+            "source_token_type": "authorization_code",
+        }
+        req = urllib.request.Request(
+            url="https://api.amazon.com/auth/token",
+            data=json.dumps(body).encode("utf-8"),
+            headers={
+                "Accept-Language": "en-US",
+                "x-amzn-identity-auth-domain": "api.amazon.com",
+                "Content-Type": "application/json",
+                "User-Agent": "Mozilla/5.0",
+            },
+            method="POST",
+        )
+        with urllib.request.urlopen(req) as r:  # noqa S310
+            if r.status != 200:
+                raise ValueError(r.read())
+            res = json.loads(r.read().decode("utf-8"))
+        try:
+            return res["access_token"]
+        except KeyError:
+            print(res)
+            raise
+
+
+def _base64_url_encode(s: bytes) -> str:
+    return base64.b64encode(s, b"-_").rstrip(b"=").decode("utf8")
+
+
+def _sha256(s: bytes) -> bytes:
+    m = hashlib.sha256()
+    m.update(s)
+    return m.digest()
---
+++ src/stkclient/client.py
@@ -0,0 +1,51 @@
+import dataclasses
+from pathlib import Path
+from typing import IO, List, Mapping
+
+@dataclasses.dataclass(frozen=True)
+class OwnedDevice:
+    """Contains details about a kindle reader device owned by the end user."""
+
+    device_capabilities: Mapping[str, bool]
+    device_name: str
+    device_serial_number: str
+
+
+class Client:
+    """Supports listing devices and sending files to specific devices."""
+
+    @staticmethod
+    def load(f: IO) -> "Client":
+        """Deserializes a client from a file-like object."""
+        pass
+
+    @staticmethod
+    def loads(s: str) -> "Client":
+        """Deserializes a client from a string."""
+        pass
+
+    def dump(self, f: IO) -> None:
+        """Serializes the client into a file-like object."""
+        pass
+
+    def dumps(self) -> str:
+        """Serializes the client into a string."""
+        pass
+
+    def get_owned_devices(self) -> List[OwnedDevice]:
+        """Returns a list of kindle devices owned by the end-user."""
+        pass
+
+    def send_file(
+        self,
+        filepath: Path,
+        target_device_serial_numbers: List[str],
+        *,
+        author: str,
+        title: str,
+        format: str,
+    ) -> None:
+        """Sends a file to the specified kindle devices."""
+        pass
+
+
